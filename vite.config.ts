7d94463b604c726460a668d87f281be8797cebf2
quycot2k3@gmail.com <jizero2003@gmail.com>
2025-06-23 11:16:10 +0700

    config base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
+  base: '/admin/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
